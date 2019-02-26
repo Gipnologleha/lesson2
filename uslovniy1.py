d97cebeff2a570cd013f6c6e90c9050fde452969
Alex Deshevykh <strider7777@yandex.ru>
2019-02-26 17:56:45 +0200

    добавил файл по условным операторам

---
+++ uslovniy1.py
@@ -0,0 +1,21 @@
+
+a = input ()
+b = input ()
+
+print (type (a))
+print (type (b))
+
+if a==b:
+    print ('true')
+else: print ('none')
+
+if len(a) == len (b):
+    print ('1')
+else: print ('0')
+
+
+if len(a) < len (b):
+    print ('3')
+else: print ('2')
+
+
